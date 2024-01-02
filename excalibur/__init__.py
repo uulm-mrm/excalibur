590103aae13ec4d65423ebd64a305084ae467003
Markus Horn <markus.horn@uni-ulm.de>
2024-01-02 10:28:24 +0100

    [FIX] matplotlib error in headless mode

--- excalibur/__init__.py
+++ excalibur/__init__.py
@@ -1,4 +1,8 @@
-import matplotlib
-matplotlib.use('tkagg')  # prevent issues of cv2 in combination with Qt platform plugin "xcb"
+try:
+    import matplotlib
+    matplotlib.use('tkagg')  # prevent issues with cv2 in combination with the Qt platform plugin "xcb"
+except ImportError:
+    import warnings
+    warnings.warn("Could not set matplotlib backend to 'tkagg'. This might cause issues with cv2 in combination with the Qt platform plugin 'xcb'.")
 
 __version__ = '0.2.0'
